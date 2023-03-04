b71eab48a91f291552034c46e3068f21fd328df9
Alex-r6 <oorlov.1986@gmail.com>
2023-03-04 23:21:19 +0200

    s_1

--- form/index.js
+++ form/index.js
@@ -1,12 +1,14 @@
 const btn = document.querySelector('.btn');
 const inp = document.querySelectorAll('.red_border');
 const box = document.querySelector('.box');
-const check = document.querySelector('.check')
+const check = document.querySelector('.check');
+const p = document.querySelector('.red')
 
 function make_border(){
 for (let index = 0; index < inp.length; index++) {
     if(inp[index].value.length === 0 || check.checked != true){
         inp[index].style.borderColor = 'red'
+        p.style.color = 'red'
     }else{
             // box.style.bottom = '0px';
             box.style.display = 'flex';
