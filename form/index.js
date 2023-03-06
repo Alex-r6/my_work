5acdf7afb5eb425d1abc41fa13575e89a5e4fced
Alex-r6 <oorlov.1986@gmail.com>
2023-03-06 22:22:37 +0200

    s_3

--- form/index.js
+++ form/index.js
@@ -4,19 +4,52 @@ const box = document.querySelector('.box');
 const check = document.querySelector('.check');
 const p = document.querySelector('.red')
 
-function make_border(){
-for (let index = 0; index < inp.length; index++) {
-    if(inp[index].value.length === 0 || check.checked != true){
-        inp[index].style.borderColor = 'red'
+let dd = 'hello' // string 
+let ddl = dd.length // number
+
+function make_border() {
+
+    // check.checked === false
+
+    let isError = false; // false // true
+
+    for (let index = 0; index < inp.length; index++) { 
+        // if (inp[index].value.length === 0) {
+        // if (inp[index].value.trim().length === 0) {
+        if (inp[index].value.trim() === '') {
+            inp[index].style.borderColor = 'red'
+            isError = true;
+        }
+        
+    }
+    
+    
+    if( check.checked === false){
         p.style.color = 'red'
-    }else{
-            // box.style.bottom = '0px';
-            box.style.display = 'flex';
-            box.innerHTML = `${inp[0].value} <br>
+        isError = true;
+    }
+
+
+
+    if (isError === false) { 
+        box.style.display = 'flex';
+        box.innerHTML = `${inp[0].value} <br>
                              ${inp[2].value}<br>
                              ${inp[4].value}`
-        }
-    } 
+    }
+
 }
 
 btn.onclick = make_border;
+// for (let index = 0; index < inp.length; index++) {
+//     if (inp[index].value.length === 0 || check.checked === false) {
+//         inp[index].style.borderColor = 'red'
+//         p.style.color = 'red'
+//     } else {
+//         // box.style.bottom = '0px';
+//         box.style.display = 'flex';
+//         box.innerHTML = `${inp[0].value} <br>
+//                          ${inp[2].value}<br>
+//                          ${inp[4].value}`
+//     }
+// }
---
+++ object/index.js
@@ -0,0 +1,96 @@
+// let user = {}
+// user.name = 'John'
+// user.surname = 'Smith'
+// user.name = 'Pete'
+// delete user.name
+// console.log(user)
+
+
+// function isEmpty (object){
+//     if ('key' in object) {
+//         console.log(true)
+//     }else{
+//         console.log(false)
+//     }
+
+// }
+// isEmpty()
+
+// function sum_salary(){
+//     let sum = 0
+//     let salaries = {
+//         John: 100,
+//         Ann: 160,
+//         Pete: 130
+//       }
+//       for(key in salaries){
+//         sum += salaries[key]
+//       }
+//       console.log(sum);
+// }
+// sum_salary()
+
+
+// function multiplyNumeric(){
+//     let menu = {
+//         width: 200,
+//         height: 300,
+//         title: "My menu"
+//       };
+//       for(key in menu){
+//         if (typeof(menu[key]) != 'string'){
+//             menu[key] *= 2
+//         }
+//       }
+//       console.log(menu);
+// }
+// multiplyNumeric()
+
+const user = {
+    emai : 'ww@ww.co,',
+    password : '',
+    message : [],
+    status : '',
+    isAdmin : false
+}
+
+const slkad = [
+    {
+        id : 'asda',
+        size : '1kg',
+        name : 'milk',
+        data : {
+            from : '02.02.2022',
+            to : '02.02.2023'
+        }
+    },
+    {
+        id : 'asd',
+        size : '4kg',
+        name : 'banana',
+        data : {
+            from : '02.02.2022',
+            to : '02.02.2023'
+        }
+    },
+]
+// slkad[0].data.from
+// innerHTML = `<div>asdasd</div>`
+
+const todo = [
+    {
+        language : 'HTML',
+        isActive : true,
+        id : 'asdasfsaasf'
+    },
+    {
+        language : 'CSS',
+        isActive : true,
+        id : 'as3dasfsaasf'
+    },
+    {
+        language : 'JS',
+        isActive : false,
+        id : '239ek2'
+    },
+]
